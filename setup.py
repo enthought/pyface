63eb4975df633b17ade0e5cfa60882b897bc7ed1
rlratzel <rlratzel@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2007-05-14 22:24:35 +0000

    * Copying new setuptools-based setup.py from enthought_split branch.

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'enthought.pyface',
+    version = '1.1.1',
+    description  = 'Traits capable windowing framework',
+    author       = 'Enthought, Inc',
+    author_email = 'info@enthought.com',
+    url          = 'http://code.enthought.com/ets',
+    license      = 'BSD',
+    zip_safe     = False,
+    packages = find_packages(),
+    include_package_data = True,
+    install_requires = [
+        "enthought.debug",
+        "enthought.logger",
+        "enthought.naming",
+        "enthought.traits",
+    ],
+    extras_require = {
+        'tvtk': ['enthought.tvtk'],
+    },
+    namespace_packages = [
+        "enthought",
+    ],
+)
