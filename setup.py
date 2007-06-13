5b52280f9ab2b3f7a4aa0fff5b2a94bc396a13a0
dpeterson <dpeterson@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2007-06-13 20:56:23 +0000

    * Bumped up version numbers to indicate these are NOT api compatible with the 2.x series.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'enthought.pyface',
-    version = '2.0.0',
+    version = '3.0.0',
     description  = 'Traits capable windowing framework',
     author       = 'Enthought, Inc',
     author_email = 'info@enthought.com',
