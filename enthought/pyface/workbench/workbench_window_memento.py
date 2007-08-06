8e6b5c75858086e4c69d30ae0ab196c7f1e02681
martin <martin@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2007-08-06 16:52:52 +0000

    - added missing file

---
+++ enthought/pyface/workbench/workbench_window_memento.py
@@ -0,0 +1,30 @@
+""" A memento for a workbench window. """
+
+
+# Enthought library imports.
+from enthought.traits.api import Any, Dict, HasTraits, Str, Tuple
+
+
+class WorkbenchWindowMemento(HasTraits):
+    """ A memento for a workbench window. """
+
+    # The Id of the active perspective.
+    active_perspective_id = Str
+
+    # The memento for the editor area.
+    editor_area_memento = Any
+
+    # Mementos for each perspective that has been seen.
+    #
+    # The keys are the perspective Ids, the values are the toolkit-specific
+    # mementos.
+    perspective_mementos = Dict(Str, Any)
+
+    # The position of the window.
+    position = Tuple
+
+    # The size of the window.
+    size = Tuple
+
+    
+#### EOF ######################################################################
