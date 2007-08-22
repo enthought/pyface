6664c5225fbdb8b801bacf44040dbcff9aaad677
dmorrill <dmorrill@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2007-08-22 00:54:34 +0000

    Added missing wx import statement.

--- enthought/pyface/grid/trait_grid_cell_adapter.py
+++ enthought/pyface/grid/trait_grid_cell_adapter.py
@@ -14,6 +14,7 @@
 """ Adapter class to make trait editor controls work inside of a grid. """
 
 # Major package imports
+import wx
 from wx.grid import PyGridCellEditor
 from wx import SIZE_ALLOW_MINUS_ONE
 
