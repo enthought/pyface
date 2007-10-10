90b34980a2aae27b4383ef877895933e4ab1eaf7
phil <phil@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2007-10-10 10:12:04 +0000

    Restructed the pyface tvtk code move all the toolkit dependent stuff into the separate backends.

--- enthought/pyface/tvtk/api.py
+++ enthought/pyface/tvtk/api.py
@@ -1,12 +1,5 @@
-# This is a temporary hack until things are properly restructured.
-from enthought.etsconfig.api import ETSConfig
+from decorated_scene import DecoratedScene
+from scene import Scene
 
-if ETSConfig.toolkit == 'qt4':
-    from decorated_scene_qt4 import DecoratedScene
-    from scene_qt4 import Scene
-else:
-    from decorated_scene import DecoratedScene
-    from scene import Scene
-
-    # Note that SimpleScene is deprecated.
-    from simple_scene import SimpleScene
+# Note that SimpleScene is deprecated.
+#from simple_scene import SimpleScene
