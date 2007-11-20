36841ce592b98ce64c16cd9c8e1d1e9058f8b374
martin <martin@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2007-11-20 20:09:29 +0000

    - replaced old view menu with the shiny new one...

--- enthought/pyface/workbench/action/view_menu_manager.py
+++ enthought/pyface/workbench/action/view_menu_manager.py
@@ -11,6 +11,7 @@ from enthought.traits.api import on_trait_change
 
 # Local imports.
 from perspective_menu_manager import PerspectiveMenuManager
+from show_view_action import ShowViewAction
 from toggle_view_visibility_action import ToggleViewVisibilityAction
 
 
@@ -66,9 +67,13 @@ class ViewMenuManager(MenuManager):
         if self.show_perspective_menu and len(self.window.perspectives) > 0:
             groups.append(Group(PerspectiveMenuManager(window=self.window)))
             
-        # Add a group containing a 'toggler' for each view.
+        # Add a group containing a 'toggler' for all visible views.
         self._view_group = self._create_view_group(self.window)
         groups.append(self._view_group)
+
+        # Add a group containing an 'Other...' item that will launch a dialog
+        # to allow the user to choose a view to show.
+        groups.append(self._create_other_group(self.window))
 
         return groups
     
@@ -76,30 +81,60 @@ class ViewMenuManager(MenuManager):
     # 'ViewMenuManager' interface.
     ###########################################################################
 
-    @on_trait_change('window.active_perspective')
-    def refresh(self):
+    @on_trait_change('window.active_perspective,window.active_part')
+    def when_active_perspective_changed(self):
         """ Refreshes the checked state of the actions in the menu. """
 
         logger.debug('refreshing view menu')
 
         if self._view_group is not None:
-            for view in self.window.views:
-                action_item = self._view_group.find(view.id)
-                action_item.action.checked = view.visible
-
+            self._clear_group(self._view_group)
+            self._initialize_view_group(self.window, self._view_group)
+            self.changed = True
+            
         return
 
     ###########################################################################
     # Private interface.
     ###########################################################################
 
+    def _clear_group(self, group):
+        """ Remove all items in a group. """
+
+        # fixme: Fix this API in PyFace so there is only one call!
+        group.destroy()
+        group.clear()
+
+        return
+            
+    def _create_other_group(self, window):
+        """ Creates a group containing the 'Other...' action. """
+
+        group = Group()
+        group.append(ShowViewAction(name='Other...', window=window))
+
+        return group
+
     def _create_view_group(self, window):
         """ Creates a group containing the view 'togglers'. """
 
         group = Group()
-        for view in window.views:
-            group.append(ToggleViewVisibilityAction(view=view, window=window))
+        self._initialize_view_group(window, group)
 
         return group
+
+    def _initialize_view_group(self, window, group):
+        """ Initializes a group containing the view 'togglers'. """
+
+        for view in window.views:
+            # fixme: It seems a little smelly to be reaching in to the window
+            # layout here. Should the 'contains_view' method be part of the
+            # window interface?
+            if window.layout.contains_view(view):
+                group.append(
+                    ToggleViewVisibilityAction(view=view, window=window)
+                )
+
+        return
 
 #### EOF ######################################################################
