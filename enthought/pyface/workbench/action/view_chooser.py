35bdee887c26ee5a029407a39f51f207b8e015bc
martin <martin@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2007-11-21 18:42:15 +0000

    - couldn't decide on sensible factoring so put all veiw chooser stuff (including the tree node subclasses) into the same module!

--- enthought/pyface/workbench/action/view_chooser.py
+++ enthought/pyface/workbench/action/view_chooser.py
@@ -2,14 +2,91 @@
 
 
 # Enthought library imports.
-from enthought.pyface.workbench.api import IView
-from enthought.traits.api import Any, HasTraits, Instance, TraitError
+from enthought.pyface.workbench.api import IView, WorkbenchWindow
+from enthought.traits.api import Any, HasTraits, Instance, List, Str
+from enthought.traits.api import TraitError, Undefined
 from enthought.traits.ui.api import Item, TreeEditor, TreeNode, View
 from enthought.traits.ui.menu import Action # fixme: Non-api import!
 
-# Local imports.
-from i_view_tree_node import IViewTreeNode
-from workbench_window_tree_node import Category, WorkbenchWindowTreeNode
+
+class Category(HasTraits):
+    """ A view category. """
+
+    # The name of the category.
+    name = Str
+
+    # The views in the category.
+    views = List
+
+
+class WorkbenchWindowTreeNode(TreeNode):
+    """ A tree node for workbench windows that displays the window's views.
+
+    The views are grouped by their category.
+
+    """
+
+    #### 'TreeNode' interface #################################################
+    
+    # List of object classes that the node applies to.
+    node_for = [WorkbenchWindow]
+
+    ###########################################################################
+    # 'TreeNode' interface.
+    ###########################################################################
+
+    def get_children(self, object):
+        """ Get the object's children. """
+
+        # Collate the window's views into categories.
+        categories_by_name = self._get_categories_by_name(object)
+
+        categories = categories_by_name.values()
+        categories.sort(key=lambda category: category.name)
+            
+        return categories
+
+    ###########################################################################
+    # Private interface.
+    ###########################################################################
+
+    def _get_categories_by_name(self, window):
+        """ Return a dictionary containing all categories keyed by name. """
+
+        categories_by_name = {}
+        for view in window.views:
+            category = categories_by_name.get(view.category)
+            if category is None:
+                category = Category(name=view.category)
+                categories_by_name[view.category] = category
+
+            category.views.append(view)
+
+        return categories_by_name
+
+
+class IViewTreeNode(TreeNode):
+    """ A tree node for objects that implement the 'IView' interface.
+
+    This node does *not* recognise objects that can be *adapted* to the 'IView'
+    interface, only those that actually implement it. If we wanted to allow
+    for adaptation we would have to work out a way for the rest of the
+    'TreeNode' code to access the adapter, not the original object. We could,
+    of course override every method, but that seems a little, errr, tedious.
+    We could probably do with something like in the PyFace tree where there
+    is a method that returns the actual object that we want to manipulate.
+
+    """
+    
+    def is_node_for(self, obj):
+        """ Returns whether this is the node that handles a specified object.
+        
+        """
+
+        # By checking for 'is obj' here, we are *not* allowing adaptation (if
+        # we were allowing adaptation it would be 'is not None'). See the class
+        # doc string for details.
+        return IView(obj, Undefined) is obj
 
 
 class ViewChooser(HasTraits):
