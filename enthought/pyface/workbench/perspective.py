76c56704142c284c3aec90848420972f74141bad
martin <martin@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-04-09 15:06:29 +0000

    - reverted code that added all views in the default perspective - it breaks the creation of new user perspectives because when you create a new one it would contain every single view instead of being empty!!!

--- enthought/pyface/workbench/perspective.py
+++ enthought/pyface/workbench/perspective.py
@@ -155,10 +155,9 @@ class Perspective(HasTraits):
         """ Adds *all* of the window's views defined in the window. """
 
         for view in window.views:
-            # Since this is the default perspective, we assume we want all of
-            # the views visible.
-            self._add_view(window, view)
-                
+            if view.visible:
+                self._add_view(window, view)
+
         return
 
     def _add_view(self, window, view):
