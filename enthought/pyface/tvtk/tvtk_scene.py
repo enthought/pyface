9f77ab7e2a3b866a1cb95dbc722aee5f296de45f
prabhu <prabhu@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-05-06 11:32:51 +0000

    ENH: Reading the back buffer when saving the scene.  This fixes some of the problems with another window occluding the scene when the scene is being saved to an image.  Thanks to Nicolas P for the patch!

--- enthought/pyface/tvtk/tvtk_scene.py
+++ enthought/pyface/tvtk/tvtk_scene.py
@@ -336,7 +336,7 @@ class TVTKScene(HasPrivateTraits):
         """Saves the rendered scene to a rasterized PostScript image.
         For vector graphics use the save_gl2ps method."""
         if len(file_name) != 0:
-            w2if = tvtk.WindowToImageFilter()
+            w2if = tvtk.WindowToImageFilter(read_front_buffer=False)
             w2if.magnification = self.magnification
             self._lift()
             w2if.input = self._renwin
@@ -348,7 +348,7 @@ class TVTKScene(HasPrivateTraits):
     def save_bmp(self, file_name):
         """Save to a BMP image file."""
         if len(file_name) != 0:
-            w2if = tvtk.WindowToImageFilter()
+            w2if = tvtk.WindowToImageFilter(read_front_buffer=False)
             w2if.magnification = self.magnification
             self._lift()
             w2if.input = self._renwin
@@ -360,7 +360,7 @@ class TVTKScene(HasPrivateTraits):
     def save_tiff(self, file_name):
         """Save to a TIFF image file."""
         if len(file_name) != 0:
-            w2if = tvtk.WindowToImageFilter()
+            w2if = tvtk.WindowToImageFilter(read_front_buffer=False)
             w2if.magnification = self.magnification
             self._lift()
             w2if.input = self._renwin
@@ -372,7 +372,7 @@ class TVTKScene(HasPrivateTraits):
     def save_png(self, file_name):
         """Save to a PNG image file."""
         if len(file_name) != 0:
-            w2if = tvtk.WindowToImageFilter()
+            w2if = tvtk.WindowToImageFilter(read_front_buffer=False)
             w2if.magnification = self.magnification
             self._lift()
             w2if.input = self._renwin
@@ -388,7 +388,7 @@ class TVTKScene(HasPrivateTraits):
         if len(file_name) != 0:
             if not quality and not progressive:
                 quality, progressive = self.jpeg_quality, self.jpeg_progressive
-            w2if = tvtk.WindowToImageFilter()
+            w2if = tvtk.WindowToImageFilter(read_front_buffer=False)
             w2if.magnification = self.magnification
             self._lift()
             w2if.input = self._renwin
