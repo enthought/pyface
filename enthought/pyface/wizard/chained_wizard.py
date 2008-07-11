029033370cb23f1843497a4b5b685848aab77add
dmartin <dmartin@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-07-11 22:49:07 +0000

    Added an if statement to the _next_wizard_changed function so that it will not try to create buttons before the control for the wizard is created.

--- enthought/pyface/wizard/chained_wizard.py
+++ enthought/pyface/wizard/chained_wizard.py
@@ -1,13 +1,13 @@
 #------------------------------------------------------------------------------
 # Copyright (c) 2005, Enthought, Inc.
 # All rights reserved.
-# 
+#
 # This software is provided without warranty under the terms of the BSD
 # license included in enthought/LICENSE.txt and may be redistributed only
 # under the conditions described in the aforementioned license.  The license
 # is also available online at http://www.enthought.com/licenses/BSD.txt
 # Thanks for using Enthought open source!
-# 
+#
 # Author: Enthought, Inc.
 # Description: <Enthought pyface package component>
 #------------------------------------------------------------------------------
@@ -53,8 +53,9 @@ class ChainedWizard(Wizard):
         if new is not None:
             self.controller.next_controller = new.controller
 
-        self._create_buttons(self.control)
-        self._update()
+        if self.control is not None:
+            self._create_buttons(self.control)
+            self._update()
 
         return
 
@@ -65,7 +66,7 @@ class ChainedWizard(Wizard):
             self.controller.next_controller = self.next_wizard.controller
 
         self._update()
-        
+
         return
-    
+
 #### EOF ######################################################################
