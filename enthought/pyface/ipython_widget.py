3a2bac74716b6e15ee04322839f342a4609356f0
gvaroquaux <gvaroquaux@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-09-05 02:00:29 +0000

    Add an ipython pyface widget.

---
+++ enthought/pyface/ipython_widget.py
@@ -0,0 +1,30 @@
+#------------------------------------------------------------------------------
+# Copyright (c) 2008, Enthought, Inc.
+# All rights reserved.
+#
+# This software is provided without warranty under the terms of the BSD
+# license included in enthought/LICENSE.txt and may be redistributed only
+# under the conditions described in the aforementioned license.  The license
+# is also available online at http://www.enthought.com/licenses/BSD.txt
+# Thanks for using Enthought open source!
+#
+# Author: Enthought, Inc.
+# Description: <Enthought pyface package component>
+#------------------------------------------------------------------------------
+""" The implementation of an IPython shell. """
+
+
+# Import the toolkit specific version.
+try:
+    import IPython.frontend
+except ImportError:
+    raise ImportError, '''
+________________________________________________________________________________
+Could not load the Wx frontend for ipython. 
+You need to have ipython >= 0.9 installed to use the ipython widget.'''
+    
+
+from toolkit import toolkit_object
+IPythonWidget= toolkit_object('ipython_widget:IPythonWidget')
+
+#### EOF ######################################################################
