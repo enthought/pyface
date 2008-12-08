feb865d95c43e1766646006c5152b9b504ee4e7f
dpeterson <dpeterson@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-12-08 23:40:53 +0000

    * Minimum version bumps after ETS 3.1.0 release.

--- setup_data.py
+++ setup_data.py
@@ -14,11 +14,11 @@ def etsdep(p, min, max=None, literal=False):
 
 
 # Declare our ETS project dependencies:
-APPTOOLS = etsdep('AppTools', '3.1.0')
-ENTHOUGHTBASE_UI = etsdep('EnthoughtBase[ui]', '3.0.1')
-TRAITS = etsdep('Traits', '3.0.3')
-TRAITSBACKENDWX = etsdep('TraitsBackendWX', '3.0.3')
-TRAITSBACKENDQT = etsdep('TraitsBackendQt', '3.0.3')
+APPTOOLS = etsdep('AppTools', '3.1.1')
+ENTHOUGHTBASE_UI = etsdep('EnthoughtBase[ui]', '3.0.2')
+TRAITS = etsdep('Traits', '3.0.4')
+TRAITSBACKENDWX = etsdep('TraitsBackendWX', '3.0.4')
+TRAITSBACKENDQT = etsdep('TraitsBackendQt', '3.0.4')
 
 
 # A dictionary of the setup data information.
@@ -55,5 +55,5 @@ INFO = {
         TRAITS
         ],
     'name': 'TraitsGUI',
-    'version': '3.0.3',
+    'version': '3.0.4',
     }
