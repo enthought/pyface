d52fc2391dd357aa65e287a18f1cb0cc7622ac0d
GaelVaroquaux <GaelVaroquaux@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2009-07-08 02:15:31 +0000

    ENH: Capture any errors in setupdocs and fail gracefully during the build.

--- setup.py
+++ setup.py
@@ -26,9 +26,8 @@ If you want to build TraitsGUI from source, you must first install
 
 """
 
-
-import os
-import zipfile
+import traceback
+import sys
 
 from distutils import log
 from distutils.command.build import build as distbuild
@@ -47,17 +46,24 @@ INFO = setup_data['INFO']
 # Pull the description values for the setup keywords from our file docstring.
 DOCLINES = __doc__.split("\n")
 
-
 class MyDevelop(develop):
     def run(self):
         develop.run(self)
-        self.run_command('build_docs')
+        try:
+            self.run_command('build_docs')
+        except:
+            log.warn("Couldn't build documentation:\n%s" %
+                     traceback.format_exception(*sys.exc_info()))
 
 
 class MyBuild(distbuild):
     def run(self):
         distbuild.run(self)
-        self.run_command('build_docs')
+        try:
+            self.run_command('build_docs')
+        except:
+            log.warn("Couldn't build documentation:\n%s" %
+                     traceback.format_exception(*sys.exc_info()))
 
 
 setup(
