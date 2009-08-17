a7ca6e697eec58ac9db90d5c04887bfe61fb3b46
epatters <epatters@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2009-08-17 15:48:26 +0000

    * Addding PythonShell and IPythonWidget demos to Pyface examples

---
+++ examples/ipython_shell.py
@@ -0,0 +1,56 @@
+#------------------------------------------------------------------------------
+#
+#  Copyright (c) 2009, Enthought, Inc.
+#  All rights reserved.
+#
+#  This software is provided without warranty under the terms of the BSD
+#  license included in enthought/LICENSE.txt and may be redistributed only
+#  under the conditions described in the aforementioned license.  The license
+#  is also available online at http://www.enthought.com/licenses/BSD.txt
+#
+#  Thanks for using Enthought open source!
+#
+#------------------------------------------------------------------------------
+""" IPython widget example. """
+
+# Enthought library imports.
+from enthought.pyface.api import ApplicationWindow, GUI
+from enthought.pyface.ipython_widget import IPythonWidget
+
+
+class MainWindow(ApplicationWindow):
+    """ The main application window. """
+
+    #### 'IWindow' interface ##################################################
+
+    # The size of the window.
+    size = (640, 480)
+
+    # The window title.
+    title = 'IPython'
+
+    ###########################################################################
+    # Protected 'IApplication' interface.
+    ###########################################################################
+
+    def _create_contents(self, parent):
+        """ Create the editor. """
+
+        self._shell = IPythonWidget(parent)
+
+        return self._shell.control
+
+
+# Application entry point.
+if __name__ == '__main__':
+    # Create the GUI.
+    gui = GUI()
+
+    # Create and open the main window.
+    window = MainWindow()
+    window.open()
+
+    # Start the GUI event loop!
+    gui.start_event_loop()
+
+##### EOF #####################################################################
---
+++ examples/python_shell.py
@@ -0,0 +1,56 @@
+#------------------------------------------------------------------------------
+#
+#  Copyright (c) 2009, Enthought, Inc.
+#  All rights reserved.
+#
+#  This software is provided without warranty under the terms of the BSD
+#  license included in enthought/LICENSE.txt and may be redistributed only
+#  under the conditions described in the aforementioned license.  The license
+#  is also available online at http://www.enthought.com/licenses/BSD.txt
+#
+#  Thanks for using Enthought open source!
+#
+#------------------------------------------------------------------------------
+""" Python shell example. """
+
+# Enthought library imports.
+from enthought.pyface.api import ApplicationWindow, GUI, PythonShell
+from enthought.pyface.action.api import Action, MenuManager, MenuBarManager
+
+
+class MainWindow(ApplicationWindow):
+    """ The main application window. """
+
+    #### 'IWindow' interface ##################################################
+
+    # The size of the window.
+    size = (640, 480)
+
+    # The window title.
+    title = 'Python'
+
+    ###########################################################################
+    # Protected 'IApplication' interface.
+    ###########################################################################
+
+    def _create_contents(self, parent):
+        """ Create the editor. """
+
+        self._shell = PythonShell(parent)
+
+        return self._shell.control
+
+
+# Application entry point.
+if __name__ == '__main__':
+    # Create the GUI.
+    gui = GUI()
+
+    # Create and open the main window.
+    window = MainWindow()
+    window.open()
+
+    # Start the GUI event loop!
+    gui.start_event_loop()
+
+##### EOF #####################################################################
