525ab5811e83cf855dded5de53a86a94dec2cf8b
ischnell <ischnell@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2010-10-15 17:59:10 +0000

    updating versions and dependencies for ETS 3.5.0 release

--- setup_data.py
+++ setup_data.py
@@ -14,11 +14,11 @@ def etsdep(p, min, max=None, literal=False):
 
 
 # Declare our ETS project dependencies:
-APPTOOLS = etsdep('AppTools', '3.3.2')
-ENTHOUGHTBASE_UI = etsdep('EnthoughtBase[ui]', '3.0.5')
-TRAITS = etsdep('Traits', '3.4.0')
-TRAITSBACKENDWX = etsdep('TraitsBackendWX', '3.4.0')
-TRAITSBACKENDQT = etsdep('TraitsBackendQt', '3.4.0')
+APPTOOLS = etsdep('AppTools', '3.4.0')
+ENTHOUGHTBASE_UI = etsdep('EnthoughtBase[ui]', '3.0.6')
+TRAITS = etsdep('Traits', '3.5.0')
+TRAITSBACKENDWX = etsdep('TraitsBackendWX', '3.5.0')
+TRAITSBACKENDQT = etsdep('TraitsBackendQt', '3.5.0')
 
 
 # A dictionary of the setup data information.
@@ -55,5 +55,5 @@ INFO = {
         TRAITS
         ],
     'name': 'TraitsGUI',
-    'version': '3.4.1',
-    }
+    'version': '3.5.0',
+}
