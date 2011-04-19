bd06e09d877b2d0ea5ace2ca01bfee5e211b8ab1
epatters <epatters@enthought.com>
2011-04-19 13:45:09 -0500

    Add mising interface to Tasks API.

--- enthought/pyface/tasks/api.py
+++ enthought/pyface/tasks/api.py
@@ -5,6 +5,7 @@ from editor import Editor
 from editor_area_pane import EditorAreaPane
 from i_dock_pane import IDockPane
 from i_editor import IEditor
+from i_editor_area_pane import IEditorAreaPane
 from i_task_pane import ITaskPane
 from task import Task
 from task_layout import TaskLayout
