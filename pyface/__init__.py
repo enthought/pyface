15365ffbb59c3ea33bf920dd8cb6687f6363f199
Ilan Schnell <ischnell@enthought.com>
2011-04-20 23:01:35 -0500

    simplified __init__.py files

--- pyface/__init__.py
+++ pyface/__init__.py
@@ -1,5 +1,5 @@
 #------------------------------------------------------------------------------
-# Copyright (c) 2005, Enthought, Inc.
+# Copyright (c) 2005-2011, Enthought, Inc.
 # All rights reserved.
 #
 # This software is provided without warranty under the terms of the BSD
@@ -14,12 +14,5 @@
 """ Reusable MVC-based components for Traits-based applications.
     Part of the TraitsGUI project of the Enthought Tool Suite.
 """
-__import__('pkg_resources').declare_namespace(__name__)
 
-# For py2app / py2exe support
-try:
-    import modulefinder
-    for p in __path__:
-        modulefinder.AddPackagePath(__name__, p)
-except:
-    pass
+__version__ = '3.7.0'
