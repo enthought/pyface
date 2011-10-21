087f7fcb360047a207622529be961644da8e6793
Mark Dickinson <mdickinson@enthought.com>
2011-10-21 11:48:29 +0100

    Revert "Fix segmentation fault in non-readonly TableEditor."
    This reverts commit ba4bea0843eac5db37e3b06d1547c707fbf90013.

--- pyface/ui/wx/grid/grid.py
+++ pyface/ui/wx/grid/grid.py
@@ -1514,21 +1514,15 @@ class _GridTableBase(PyGridTableBase):
         self._renderer_cache = {}
 
     def dispose(self):
+
         # Make sure dispose gets called on all traits editors:
-
-        # The wx grid machinery can end up calling GetAttr one more time after
-        # this call, so delay emptying of the editor cache.  See:
-        # https://github.com/enthought/traitsui/issues/2
-
-        do_later(self._empty_editor_cache)
-        for renderer in self._renderer_cache.values():
-            renderer.dispose()
-        self._renderer_cache = {}
-
-    def _empty_editor_cache(self):
         for editor in self._editor_cache.values():
             editor.dispose()
         self._editor_cache = {}
+
+        for renderer in self._renderer_cache.values():
+            renderer.dispose()
+        self._renderer_cache = {}
 
     ###########################################################################
     # 'wxPyGridTableBase' interface.
