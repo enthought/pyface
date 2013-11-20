7bc85948687377f3e40a7a1e9e5ce112042a90ab
Pietro Berkes <pberkes@enthought.com>
2013-11-20 13:52:35 +0000

    Remove redundancies from EnamlTaskPane and EnamlEditor.

--- pyface/tasks/enaml_editor.py
+++ pyface/tasks/enaml_editor.py
@@ -1,70 +1,7 @@
-# Enthought library imports.
-from traits.api import Instance
-from enaml.widgets.toolkit_object import ToolkitObject
-
 # local imports
 from pyface.tasks.editor import Editor
+from pyface.tasks.enaml_pane import EnamlPane
 
 
-class EnamlEditor(Editor):
+class EnamlEditor(EnamlPane, Editor):
     """ Create an Editor for Enaml Components. """
-
-    ###########################################################################
-    # 'EnamlEditor' interface
-    ###########################################################################
-
-    #: The Enaml component defining the contents of the Editor.
-    component = Instance(ToolkitObject)
-
-    def create_component(self):
-        """ Return an Enaml component defining the contents of the Editor.
-
-        Returns
-        -------
-        component : ToolkitObject
-        """
-        raise NotImplementedError
-
-    ###########################################################################
-    # 'IEditor' interface.
-    ###########################################################################
-
-    def create(self, parent):
-        """ Create the toolkit-specific control that represents the editor. """
-
-        self.component = self.create_component()
-
-        # We start with an invisible component to avoid flicker. We restore the
-        # initial state after the Qt control is parented.
-        visible = self.component.visible
-        self.component.visible = False
-
-        # Initialize the proxy.
-        self.component.initialize()
-
-        # Activate the proxy.
-        if not self.component.proxy_is_active:
-            self.component.activate_proxy()
-
-        # Fish the Qt control out of the proxy. That's our TaskPane content.
-        self.control = self.component.proxy.widget
-
-        # Set the parent
-        if parent is not None:
-            self.control.setParent(parent)
-
-        # Restore the visibility state
-        self.component.visible = visible
-        self.component.proxy.relayout()
-
-    def destroy(self):
-        """ Destroy the toolkit-specific control that represents the editor.
-        """
-
-        control = self.control
-        if control is not None:
-            control.hide()
-            self.component.destroy()
-            control.deleteLater()
-
-        self.control = None
---
+++ pyface/tasks/enaml_pane.py
@@ -0,0 +1,69 @@
+""" Base class defining common code for EnamlTaskPane and EnamlEditor. """
+
+# Enthought library imports.
+from enaml.widgets.toolkit_object import ToolkitObject
+from traits.api import HasTraits, Instance
+
+
+class EnamlPane(HasTraits):
+    """ Base class defining common code for EnamlTaskPane and EnamlEditor. """
+
+    ###########################################################################
+    # 'EnamlPane' interface
+    ###########################################################################
+
+    #: The Enaml component defining the contents of the TaskPane.
+    component = Instance(ToolkitObject)
+
+    def create_component(self):
+        """ Return an Enaml component defining the contents of the pane.
+
+        Returns
+        -------
+        component : ToolkitObject
+        """
+        raise NotImplementedError
+
+    ###########################################################################
+    # 'TaskPane'/'Editor' interface
+    ###########################################################################
+
+    def create(self, parent):
+        """ Create the toolkit-specific control that represents the editor. """
+
+        self.component = self.create_component()
+
+        # We start with an invisible component to avoid flicker. We restore the
+        # initial state after the Qt control is parented.
+        visible = self.component.visible
+        self.component.visible = False
+
+        # Initialize the proxy.
+        self.component.initialize()
+
+        # Activate the proxy.
+        if not self.component.proxy_is_active:
+            self.component.activate_proxy()
+
+        # Fish the Qt control out of the proxy. That's our TaskPane content.
+        self.control = self.component.proxy.widget
+
+        # Set the parent
+        if parent is not None:
+            self.control.setParent(parent)
+
+        # Restore the visibility state
+        self.component.visible = visible
+        self.component.proxy.relayout()
+
+    def destroy(self):
+        """ Destroy the toolkit-specific control that represents the editor.
+        """
+
+        control = self.control
+        if control is not None:
+            control.hide()
+            self.component.destroy()
+            control.deleteLater()
+
+        self.control = None
