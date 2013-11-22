258519a8d4440c7024f0526ecc43c1d2f63da59a
Pietro Berkes <pberkes@enthought.com>
2013-11-22 10:55:45 +0000

    Update the enaml-based example.

--- examples/tasks/enaml/run.py
+++ examples/tasks/enaml/run.py
@@ -1,3 +1,8 @@
+#------------------------------------------------------------------------------
+# Copyright (c) 2013, Enthought, Inc.
+# All rights reserved.
+#------------------------------------------------------------------------------
+
 # Enthought library imports.
 from pyface.api import GUI
 from pyface.tasks.api import TaskWindow
@@ -10,6 +15,12 @@ def main(argv):
     """ A simple example of using Tasks.
     """
     # Create the GUI (this does NOT start the GUI event loop).
+    from traits.etsconfig.api import ETSConfig
+    ETSConfig.toolkit = 'qt4'
+
+    from enaml.qt.qt_application import QtApplication
+    app = QtApplication()
+
     gui = GUI()
 
     # Create a Task and add it to a TaskWindow.
