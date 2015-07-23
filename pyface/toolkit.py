e6f971ab11ed97a7cf1b550a15319dc98e523fdf
Corran Webster <cwebster@enthought.com>
2015-07-23 10:24:34 +0100

    Don't swallow import errors for other modules.

--- pyface/toolkit.py
+++ pyface/toolkit.py
@@ -100,7 +100,11 @@ def toolkit_object(name):
             be_obj = getattr(sys.modules[be_mname], oname)
         except AttributeError:
             pass
-    except ImportError, e:
+    except ImportError as exc:
+        # is the error while trying to import be_mname or not?
+        if not exc.message.endswith(be_mname):
+            # something else went wrong - let the exception be raised
+            raise
 
         # Ignore *ANY* errors unless a debug ENV variable is set.
         if 'ETS_DEBUG' in os.environ:
