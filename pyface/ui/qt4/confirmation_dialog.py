b0d4826b4c11996e8df8c0302c7d2b3017e54a3d
Corran Webster <cwebster@enthought.com>
2015-07-25 09:49:39 +0100

    Fix missing import.

--- pyface/ui/qt4/confirmation_dialog.py
+++ pyface/ui/qt4/confirmation_dialog.py
@@ -21,7 +21,7 @@ from traits.api import Bool, Dict, Enum, Instance, provides, Unicode
 from pyface.i_confirmation_dialog import IConfirmationDialog, MConfirmationDialog
 from pyface.constant import CANCEL, YES, NO
 from pyface.image_resource import ImageResource
-from dialog import Dialog
+from dialog import Dialog, _RESULT_MAP
 
 
 @provides(IConfirmationDialog)
