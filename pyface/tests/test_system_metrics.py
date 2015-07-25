dd3da295eedb3afa192610d09ef69ca8c84bffa1
Corran Webster <cwebster@enthought.com>
2015-07-25 17:11:58 +0100

    Tests for system metrics.

---
+++ pyface/tests/test_system_metrics.py
@@ -0,0 +1,24 @@
+from __future__ import absolute_import
+
+from traits.testing.unittest_tools import unittest
+
+from ..system_metrics import SystemMetrics
+
+
+class TestSystemMetrics(unittest.TestCase):
+
+    def setUp(self):
+        self.metrics = SystemMetrics()
+
+    def test_width(self):
+        width = self.metrics.screen_width
+        self.assertGreaterEqual(width, 0)
+
+    def test_height(self):
+        height = self.metrics.screen_height
+        self.assertGreaterEqual(height, 0)
+
+    def test_background_color(self):
+        color = self.metrics.dialog_background_color
+        self.assertIn(len(color), [3, 4])
+        self.assertTrue(all(0 <= channel <= 1 for channel in color))
