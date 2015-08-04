c77781d4ecf360c00d871d3f90cd5788d74d6400
Ioannis Tziakos <ioannist@enthought.com>
2015-08-04 10:29:35 +0100

    try a differentr path separator

--- appveyor-test.cmd
+++ appveyor-test.cmd
@@ -1,6 +1,5 @@
-set ETS_TOOLKIT=qt4
 coverage run -m nose.core -v --exclude-dir=pyface/ui/wx --exclude="enaml"
 if %errorlevel% neq 0 exit /b %errorlevel%
-coverage run -a -m nose.core -v pyface/tasks/tests/test_enaml*
+coverage run -a -m nose.core -v pyface\tasks\tests\test_enaml*
 if %errorlevel% neq 0 exit /b %errorlevel%
 coverage report
