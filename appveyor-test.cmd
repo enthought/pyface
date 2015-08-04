49599601a20edad90e280335090c953a6c621c59
Ioannis Tziakos <ioannist@enthought.com>
2015-08-04 10:52:32 +0100

    exclude tests based on ETS_TOOLKIT

--- appveyor-test.cmd
+++ appveyor-test.cmd
@@ -1,3 +1,6 @@
-coverage run -m nose.core -v --exclude-dir=pyface/ui/wx
+if %ETS_TOOLKIT%==qt4 (
+   coverage run -m nose.core -v --exclude-dir=pyface/ui/wx
+) else (
+   coverage run -m nose.core -v --exclude-dir=pyface/ui/qt
 if %errorlevel% neq 0 exit /b %errorlevel%
 coverage report
