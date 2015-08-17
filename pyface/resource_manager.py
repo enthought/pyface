b7aa5372940e2afc71e86846118e1b86672ffaa8
Corran Webster <cwebster@enthought.com>
2015-08-17 15:50:31 +0100

    Fix __future__

--- pyface/resource_manager.py
+++ pyface/resource_manager.py
@@ -13,14 +13,12 @@
 #------------------------------------------------------------------------------
 """ The implementation of a shared resource manager. """
 
+from __future__ import absolute_import
 
 # Enthought library imports.
 from pyface.resource.api import ResourceManager
 
-
 # Import the toolkit specific version.
-from __future__ import absolute_import
-
 from .toolkit import toolkit_object
 PyfaceResourceFactory = toolkit_object('resource_manager:PyfaceResourceFactory')
 
