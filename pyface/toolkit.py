404835a98840aff69d8648bf93523ce9e58f1bf6
Corran Webster <cwebster@enthought.com>
2015-08-18 13:26:01 +0100

    Change tests for match with module name in toolkit import.

--- pyface/toolkit.py
+++ pyface/toolkit.py
@@ -106,7 +106,7 @@ def toolkit_object(name):
             pass
     except ImportError as exc:
         # is the error while trying to import be_mname or not?
-        if not exc.args[0].endswith(mname):
+        if mname not in exc.args[0]:
             # something else went wrong - let the exception be raised
             raise
 
