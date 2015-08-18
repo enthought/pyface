8a71d8c38ef4d64dd28f169c5b3335f964e81156
Ioannis Tziakos <ioannist@enthought.com>
2015-08-18 11:18:38 +0100

    do not install enaml and traits enaml on python 3

--- appveyor-install.cmd
+++ appveyor-install.cmd
@@ -3,15 +3,16 @@ call setenv /x64
 
 rem install python packages
 pip install --cache-dir c:/temp nose
+pip install --cache-dir c:/temp coverage
 pip install --cache-dir c:/temp nose-exclude
 pip install --cache-dir c:/temp mock
 pip install --cache-dir c:/temp pyside
 pip install --cache-dir c:/temp pygments
 pip install --cache-dir c:/temp git+http://github.com/enthought/traits.git#egg=traits
 pip install --cache-dir c:/temp git+http://github.com/enthought/traitsui.git@feature/python3#egg=traitsui
-pip install --cache-dir c:/temp traits_enaml
-pip install --cache-dir c:/temp enaml
-pip install --cache-dir c:/temp coverage
+if %PYTHON% EQU "C:/Python34-x64" (
+   pip install --cache-dir c:/temp traits_enaml
+   pip install --cache-dir c:/temp enaml )
 
 rem install pyface
 python setup.py develop
