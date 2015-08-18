5805580e503ca675f613cd7c766c1f24b49ab86f
Ioannis Tziakos <ioannist@enthought.com>
2015-08-18 11:31:07 +0100

    fix paths and do not run the enaml tests on python 3

--- appveyor-test.cmd
+++ appveyor-test.cmd
@@ -2,5 +2,9 @@ mkdir testrun
 copy .coveragerc testrun/
 cd testrun
 if %errorlevel% neq 0 exit /b %errorlevel%
+if %PYTHON% EQU "C:/Python27-x64" (
+   coverage run -m nose.core -v ../ --exclude-dir=../pyface/ui/wx)
+else (
+   coverage run -m nose.core -v ../ --exclude-dir=../pyface/ui/wx --exclude="enaml")
 if %errorlevel% neq 0 exit /b %errorlevel%
 coverage report --omit=*wx*
