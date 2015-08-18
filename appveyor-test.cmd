6eed987f488d424209fe283306b7eacaaf93d725
itziakos <ioannist@enthought.com>
2015-08-18 13:14:58 +0100

    simplify test command

--- appveyor-test.cmd
+++ appveyor-test.cmd
@@ -2,8 +2,6 @@ mkdir testrun
 copy .coveragerc testrun/
 cd testrun
 if %errorlevel% neq 0 exit /b %errorlevel%
-IF %PYTHON% EQU "C:/Python27-x64" coverage run -m nose.core -v ../ --exclude-dir=../pyface/ui/wx
-IF %PYTHON% EQU "C:/Python34-x64" coverage run -m nose.core -v ../ --exclude-dir=../pyface/ui/wx --exclude="enaml"
-
+coverage run -m nose.core pyface -v --exclude='wx'
 if %errorlevel% neq 0 exit /b %errorlevel%
 coverage report --omit=*wx*
