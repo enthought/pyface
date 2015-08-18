1154caf2c8e021bf3bbc3882cd38de7ad30ea5a0
Ioannis Tziakos <ioannist@enthought.com>
2015-08-18 11:30:21 +0100

    run the tests in a subfolder

--- appveyor-test.cmd
+++ appveyor-test.cmd
@@ -1,3 +1,6 @@
-coverage run -m nose.core -v --exclude-dir=pyface/ui/wx
+mkdir testrun
+copy .coveragerc testrun/
+cd testrun
+if %errorlevel% neq 0 exit /b %errorlevel%
 if %errorlevel% neq 0 exit /b %errorlevel%
 coverage report --omit=*wx*
