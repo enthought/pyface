edf885862fa6ac5dea42f87853c89eb0f6d5a09a
Corran Webster <cwebster@enthought.com>
2015-08-20 20:36:29 +0100

    Don't use 'next' fixer from 2to3 for pyface.

--- setup.py
+++ setup.py
@@ -139,4 +139,7 @@ if __name__ == "__main__":
           platforms=["Windows", "Linux", "Mac OS-X", "Unix", "Solaris"],
           zip_safe=False,
           use_2to3=True,
+          use_2to3_exclude_fixers=[
+            'lib2to3.fixes.next',  # we have several .next() methods, no iterators
+          ],
     )
