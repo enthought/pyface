8a3cffe2734222b1cd6c585b59aae6c8ffa66e5c
Gregor Thalhammer <gregor.thalhammer@gmail.com>
2015-09-30 17:23:01 +0200

    add Qt5 support to pyface.qt import shims

--- pyface/qt/QtNetwork.py
+++ pyface/qt/QtNetwork.py
@@ -3,5 +3,8 @@ from . import qt_api
 if qt_api == 'pyqt':
     from PyQt4.QtNetwork import *
     
+elif qt_api == 'pyqt5':
+    from PyQt5.QtNetwork import *
+    
 else:
     from PySide.QtNetwork import *
