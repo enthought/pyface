d24693e9ddb617285424417fef7e5b53d3dc779d
Stefano Borini <sborini@enthought.com>
2016-03-22 13:31:07 +0000

    Minor adjustments to test

--- pyface/ui/qt4/workbench/tests/test_workbench_window_layout.py
+++ pyface/ui/qt4/workbench/tests/test_workbench_window_layout.py
@@ -12,8 +12,15 @@ class TestWorkbenchWindowLayout(unittest.TestCase):
     def test_change_of_active_qt_editor(self):
         # Test error condition for enthought/mayavi#321
 
-        layout = WorkbenchWindowLayout(
-            _qt4_editor_area=mock.Mock(
-                spec=SplitTabWidget))
+        mock_split_tab_widget = mock.Mock(spec=SplitTabWidget)
 
+        layout = WorkbenchWindowLayout(_qt4_editor_area=mock_split_tab_widget)
+
+        # This should not throw
         layout._qt4_active_editor_changed(None, None)
+        self.assertEqual(mock_split_tab_widget.setTabTextColor.called, False)
+
+        mock_active_editor = mock.Mock()
+        layout._qt4_active_editor_changed(None, mock_active_editor)
+
+        self.assertEqual(mock_split_tab_widget.setTabTextColor.called, True)
