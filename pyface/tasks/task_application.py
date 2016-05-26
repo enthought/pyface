30df7b0dcc3b33108c568feba32d463f6e929402
Corran Webster <cwebster@enthought.com>
2016-05-26 10:46:00 +0100

    Super call when closing window.

--- pyface/tasks/task_application.py
+++ pyface/tasks/task_application.py
@@ -65,13 +65,13 @@ class TaskApplication(Application):
     # -------------------------------------------------------------------------
 
     # Destruction utilities ---------------------------------------------------
+
     def _on_window_closed(self, window, trait, old, new):
         """ Listener that ensures window handles are released when closed.
         """
         if window.active_task in self.tasks:
             self.tasks.remove(window.active_task)
-
-        self.windows.remove(window)
+        super(TaskApplication, self)._on_window_close(window, trait, old, new)
 
     # Trait initializers and property getters ---------------------------------
 
