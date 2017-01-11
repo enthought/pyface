fdda2c0935d2255dbc7a96879ae10fc2953508aa
Corran Webster <cwebster@enthought.com>
2017-01-11 09:42:36 +0000

    Need to install invoke into default edm environment.

--- appveyor-bootstrap-edm.cmd
+++ appveyor-bootstrap-edm.cmd
@@ -7,6 +7,10 @@ IF /I "%1" == "setup" GOTO setup
 :setup		
 CALL powershell.exe -Command "(new-object net.webclient).DownloadFile('https://package-data.enthought.com/edm/win_x86_64/1.4/edm_1.4.0_x86_64.msi', 'edm.msi')"		
 msiexec /qn /a edm.msi TARGETDIR=c:\		
+
+rem install pip and invoke into default EDM environment
+edm install -y pip invoke
+
 GOTO end		
 		
 		
