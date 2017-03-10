03ae425efa15b9d143426470a4f031b98836ab07
Corran Webster <cwebster@enthought.com>
2017-03-10 16:21:11 +0000

    Missing 's' in import.

--- pyface/qt/QtGui.py
+++ pyface/qt/QtGui.py
@@ -6,7 +6,7 @@ if qt_api == 'pyqt':
 
 elif qt_api == 'pyqt5':
     from PyQt5.QtGui import *
-    from PyQt5.QtWidget import *
+    from PyQt5.QtWidgets import *
     from PyQt5.QtPrintSupport import *
     from PyQt5.QtCore import (
         QAbstractProxyModel, QItemSelection, QItemSelectionModel,
