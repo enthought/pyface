bd44cda0e42dae215c180ac0310f86ba6e5fb2e0
Simon Jagoe <sjagoe@enthought.com>
2017-07-06 05:48:43 -0700

    Add missing QtWebkit import for PyQt5 compatibility

--- pyface/qt/QtWebKit.py
+++ pyface/qt/QtWebKit.py
@@ -4,6 +4,7 @@ if qt_api == 'pyqt':
     from PyQt4.QtWebKit import *
 
 elif qt_api == 'pyqt5':
+    from PyQt5.QtWebKit import *
     from PyQt5.QtWidgets import *
     from PyQt5.QtWebKitWidgets import *
 
