ac76443791be5525e80543b9f911acfd838839e1
Poruri Sai Rahul <rporuri@enthought.com>
2019-02-16 22:28:40 +0530

    FIX : Fix the Sequence import
    For now, the Sequence import should be collections.Sequence,
    which might need to change in future versions of Python
    
    	modified:   pyface/resource/resource_manager.py

--- pyface/resource/resource_manager.py
+++ pyface/resource/resource_manager.py
@@ -55,7 +55,7 @@ class ResourceManager(HasTraits):
     def locate_image(self, image_name, path, size=None):
         """ Locates an image. """
 
-        if not isinstance(path, collections.abc.Sequence):
+        if not isinstance(path, collections.Sequence):
             path = [path]
 
         resource_path = []
