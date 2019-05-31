3dddeebbc36deb41f4a0db1c632db7549f7fddd0
Corran Webster <cwebster@enthought.com>
2019-05-31 17:04:04 +0100

    Add tests for do_later and do_after.

--- pyface/timer/tests/test_do_later.py
+++ pyface/timer/tests/test_do_later.py
@@ -5,7 +5,7 @@ from unittest import TestCase, skipIf
 
 from pyface.toolkit import toolkit_object
 from ..i_timer import perf_counter
-from ..do_later import DoLaterTimer
+from ..do_later import DoLaterTimer, do_after, do_later
 
 GuiTestAssistant = toolkit_object('util.gui_test_assistant:GuiTestAssistant')
 no_gui_test_assistant = (GuiTestAssistant.__name__ == 'Unimplemented')
@@ -15,23 +15,15 @@ class ConditionHandler(object):
     def __init__(self):
         self.count = 0
         self.times = []
-        self.called = False
 
     def callback(self):
         self.times.append(perf_counter())
         self.count += 1
-        self.called = True
-
-    def is_called(self):
-        return self.called
-
-    def called_n(self, repeat):
-        return lambda: self.count >= repeat
 
 
 @skipIf(no_gui_test_assistant, 'No GuiTestAssistant')
-class TestCallbackTimer(TestCase, GuiTestAssistant):
-    """ Test the CallbackTimer. """
+class TestDoLaterTimer(TestCase, GuiTestAssistant):
+    """ Test the DoLaterTimer. """
 
     def setUp(self):
         GuiTestAssistant.setUp(self)
@@ -52,6 +44,65 @@ class TestCallbackTimer(TestCase, GuiTestAssistant):
         finally:
             timer.stop()
 
+        self.assertEqual(handler.count, 1)
+
+        expected_time = timer._start_time + 0.10
+
+        # give feedback in case of failure
+        if expected_time > handler.times[0]:
+            print(handler.times)
+
+        self.assertTrue(expected_time <= handler.times[0])
+
+
+@skipIf(no_gui_test_assistant, 'No GuiTestAssistant')
+class TestDoLater(TestCase, GuiTestAssistant):
+    """ Test do_later. """
+
+    def setUp(self):
+        GuiTestAssistant.setUp(self)
+
+    def tearDown(self):
+        GuiTestAssistant.tearDown(self)
+
+    def test_basic(self):
+        handler = ConditionHandler()
+        timer = do_later(handler.callback)
+
+        try:
+            self.assertTrue(timer.active)
+            self.event_loop_helper.event_loop_until_condition(
+                lambda: not timer.active
+            )
+            self.assertFalse(timer.active)
+        finally:
+            timer.stop()
+
+        self.assertEqual(handler.count, 1)
+
+
+@skipIf(no_gui_test_assistant, 'No GuiTestAssistant')
+class TestDoAfter(TestCase, GuiTestAssistant):
+    """ Test do_after. """
+
+    def setUp(self):
+        GuiTestAssistant.setUp(self)
+
+    def tearDown(self):
+        GuiTestAssistant.tearDown(self)
+
+    def test_basic(self):
+        handler = ConditionHandler()
+        timer = do_after(100, handler.callback)
+
+        try:
+            self.assertTrue(timer.active)
+            self.event_loop_helper.event_loop_until_condition(
+                lambda: not timer.active
+            )
+            self.assertFalse(timer.active)
+        finally:
+            timer.stop()
 
         self.assertEqual(handler.count, 1)
 
