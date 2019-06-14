76c3cb7c540f0d8c56b3c6aa6851fb36890568ae
Corran Webster <cwebster@enthought.com>
2019-06-14 08:47:10 +0100

    Remove use of cmp in fix_introspect_bug. (#395)

--- pyface/util/fix_introspect_bug.py
+++ pyface/util/fix_introspect_bug.py
@@ -78,7 +78,7 @@ def getAttributeNames(object, includeMagic=1, includeSingle=1,
     # e.g. ITK http://www.itk.org/
     attributes = [attribute for attribute in attributes \
                   if isinstance(attribute, six.string_types)]
-    attributes.sort(lambda x, y: cmp(x.upper(), y.upper()))
+    attributes.sort(key=lambda x: x.upper())
     if not includeSingle:
         attributes = filter(lambda item: item[0]!='_' \
                             or item[1]=='_', attributes)
