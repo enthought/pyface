f007bf68273abd814895e30d1eca8d85e347ba66
Corran Webster <cwebster@enthought.com>
2019-06-14 11:27:09 +0100

    Fix imports for pyqt5 with webkit (#396)
    * Fix imports for pyqt5 with webkit (rather than webengine).
    
    * Fix login in QtScript import.

--- pyface/qt/QtScript.py
+++ pyface/qt/QtScript.py
@@ -3,11 +3,11 @@ from . import qt_api
 if qt_api == 'pyqt':
     from PyQt4.QtScript import *
 
-if qt_api == 'pyqt5':
+elif qt_api == 'pyqt5':
     import warnings
     warnings.warn(DeprecationWarning("QtScript is not supported in PyQt5"))
 
-if qt_api == 'pyside2':
+elif qt_api == 'pyside2':
     import warnings
     warnings.warn(DeprecationWarning("QtScript is not supported in PyQt5"))
 
