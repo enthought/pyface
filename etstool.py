15fe9a2801fc481d6315726cab7ebeefe1e3df8b
Corran Webster <cwebster@enthought.com>
2020-04-28 16:30:08 +0100

    Quote wx version spec for windows, better check for pyside in image tests.

--- etstool.py
+++ etstool.py
@@ -166,12 +166,12 @@ def install(edm, runtime, toolkit, environment):
     elif toolkit == "wx":
         if sys.platform != "linux":
             commands.append(
-                "{edm} run -e {environment} -- pip install wxPython<4.1"
+                "{edm} run -e {environment} -- pip install 'wxPython<4.1'"
             )
         else:
             # XXX this is mainly for TravisCI workers; need a generic solution
             commands.append(
-                "{edm} run -e {environment} -- pip install -f https://extras.wxpython.org/wxPython4/extras/linux/gtk3/ubuntu-16.04/ wxPython<4.1"
+                "{edm} run -e {environment} -- pip install -f https://extras.wxpython.org/wxPython4/extras/linux/gtk3/ubuntu-16.04/ 'wxPython<4.1'"
             )
 
     click.echo("Creating environment '{environment}'".format(**parameters))
