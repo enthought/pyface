b149f5f88913480f822bec128f108782e116100c
Corran Webster <cwebster@enthought.com>
2020-05-11 14:51:17 +0100

    Remove PyQt4 and PySide from CI (#512)
    * Remove old qt versions; fix mock imports; remove nose.
    
    * Update appveyor and travis config.
    
    * Clean-up etstool.
    
    * Remove pyside and pyqt4 from pyface.qt and pyface extra requirements.
    
    * Go back to using nose for now.
    
    * Fix typo in QtNetwork.py
    
    * Properly fix typo in QtNetwork.py
    
    Note - do not try to fix typos while managing small children.
    
    * Change URL for linux wxPython wheels.
    
    * Test on wxPython < 4.1.  Possibly some issues with wxPython.org.
    
    * Quote wx version spec for windows, better check for pyside in image tests.
    
    * More messing around with wxPython install commands.
    
    * Break down wxPython installs by system.
    
    * Clean-up tests and other places that use PyQt4.
    
    * Fix tests, add QWidgets to qt api.
    
    * Remove unused imports.

--- pyface/qt/QtOpenGL.py
+++ pyface/qt/QtOpenGL.py
@@ -9,14 +9,8 @@
 # Thanks for using Enthought open source!
 from . import qt_api
 
-if qt_api == "pyqt":
-    from PyQt4.QtOpenGL import *
-
-elif qt_api == "pyqt5":
+if qt_api == "pyqt5":
     from PyQt5.QtOpenGL import *
 
-elif qt_api == "pyside2":
-    from PySide2.QtOpenGL import *
-
 else:
-    from PySide.QtOpenGL import *
+    from PySide2.QtOpenGL import *
--- pyface/qt/QtSvg.py
+++ pyface/qt/QtSvg.py
@@ -9,14 +9,8 @@
 # Thanks for using Enthought open source!
 from . import qt_api
 
-if qt_api == "pyqt":
-    from PyQt4.QtSvg import *
-
-elif qt_api == "pyqt5":
+if qt_api == "pyqt5":
     from PyQt5.QtSvg import *
 
-elif qt_api == "pyside2":
-    from PySide2.QtSvg import *
-
 else:
-    from PySide.QtSvg import *
+    from PySide2.QtSvg import *
