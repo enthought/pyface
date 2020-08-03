a64dece083091b56af1705f6b2e4e221f6061a4e
Aaron Ayres <aayres@enthought.com>
2020-08-03 08:19:12 -0500

    replace GetClippingBox with GetClippingRect (returns the only the needed arguments for SetClippingRegion)

--- pyface/dock/dock_sizer.py
+++ pyface/dock/dock_sizer.py
@@ -622,7 +622,7 @@ class DockItem(HasPrivateTraits):
     def begin_draw(self, dc, ox=0, oy=0):
         """ Prepares for drawing into a device context.
         """
-        self._save_clip = dc.GetClippingBox()
+        self._save_clip = dc.GetClippingRect()
         x, y, dx, dy = self.bounds
         dc.SetClippingRegion(x + ox, y + oy, dx, dy)
 
@@ -635,7 +635,7 @@ class DockItem(HasPrivateTraits):
         """
         dc.DestroyClippingRegion()
         if self._save_clip != no_clip:
-            dc.SetClippingRegion(*self._save_clip[1:])
+            dc.SetClippingRegion(*self._save_clip)
         self._save_clip = None
 
     # ---------------------------------------------------------------------------
