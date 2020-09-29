5ad12378e0f1b72ed38b7fce25b9064dd2e0f4c2
Corran Webster <cwebster@enthought.com>
2020-09-29 11:10:12 +0100

    Clean up column example to use shared data generation.

--- examples/data_view/column_example.py
+++ examples/data_view/column_example.py
@@ -10,8 +10,8 @@
 
 """ Example showing DataView for ColumnDataModel using row info. """
 
-
 from functools import partial
+import logging
 from random import choice, randint, uniform
 
 from traits.api import Bool, Dict, HasStrictTraits, Instance, Int, Str, List
@@ -27,6 +27,12 @@ from pyface.data_view.value_types.api import (
 from column_data_model import (
     AbstractRowInfo, ColumnDataModel, HasTraitsRowInfo
 )
+from example_data import (
+    any_name, family_name, favorite_color, age, street, city, country
+)
+
+logger = logging.getLogger(__name__)
+
 
 flags = {
     'UK': ImageResource('gb.png'),
@@ -141,98 +147,39 @@ class MainWindow(ApplicationWindow):
         return self.data_view.control
 
     def _data_default(self):
-        import numpy
-        return numpy.random.uniform(size=(100000, 10))
+        logger.info("Initializing data")
+        people = [
+            Person(
+                name='%s %s' % (any_name(), family_name()),
+                age=age(),
+                favorite_color=favorite_color(),
+                address=Address(
+                    street=street(),
+                    city=city(),
+                    country=country(),
+                ),
+            )
+            for i in range(100)
+        ]
+        logger.info("Data initialized")
+        return people
 
     def destroy(self):
         self.data_view.destroy()
         super().destroy()
 
 
-male_names = [
-    'Michael', 'Edward', 'Timothy', 'James', 'George', 'Ralph', 'David',
-    'Martin', 'Bryce', 'Richard', 'Eric', 'Travis', 'Robert', 'Bryan',
-    'Alan', 'Harold', 'John', 'Stephen', 'Gael', 'Frederic', 'Eli', 'Scott',
-    'Samuel', 'Alexander', 'Tobias', 'Sven', 'Peter', 'Albert', 'Thomas',
-    'Horatio', 'Julius', 'Henry', 'Walter', 'Woodrow', 'Dylan', 'Elmer',
-]
-
-female_names = [
-    'Leah', 'Jaya', 'Katrina', 'Vibha', 'Diane', 'Lisa', 'Jean', 'Alice',
-    'Rebecca', 'Delia', 'Christine', 'Marie', 'Dorothy', 'Ellen', 'Victoria',
-    'Elizabeth', 'Margaret', 'Joyce', 'Sally', 'Ethel', 'Esther', 'Suzanne',
-    'Monica', 'Hortense', 'Samantha', 'Tabitha', 'Judith', 'Ariel', 'Helen',
-    'Mary', 'Jane', 'Janet', 'Jennifer', 'Rita', 'Rena', 'Rianna',
-]
-
-all_names = male_names + female_names
-any_name = partial(choice, all_names)
-age = partial(randint, 15, 72)
-
-
-def favorite_color():
-    return Color(hsv=(uniform(0.0, 1.0), 1.0, 1.0))
-
-
-def family_name():
-    return choice([
-        'Jones', 'Smith', 'Thompson', 'Hayes', 'Thomas', 'Boyle', "O'Reilly",
-        'Lebowski', 'Lennon', 'Starr', 'McCartney', 'Harrison', 'Harrelson',
-        'Steinbeck', 'Rand', 'Hemingway', 'Zhivago', 'Clemens', 'Heinlien',
-        'Farmer', 'Niven', 'Van Vogt', 'Sturbridge', 'Washington', 'Adams',
-        'Bush', 'Kennedy', 'Ford', 'Lincoln', 'Jackson', 'Johnson',
-        'Eisenhower', 'Truman', 'Roosevelt', 'Wilson', 'Coolidge', 'Mack',
-        'Moon', 'Monroe', 'Springsteen', 'Rigby', "O'Neil", 'Philips',
-        'Clinton', 'Clapton', 'Santana', 'Midler', 'Flack', 'Conner', 'Bond',
-        'Seinfeld', 'Costanza', 'Kramer', 'Falk', 'Moore', 'Cramdon', 'Baird',
-        'Baer', 'Spears', 'Simmons', 'Roberts', 'Michaels', 'Stuart',
-        'Montague', 'Miller',
-    ])
-
-
-def street():
-    number = randint(11, 999)
-    text_1 = choice([
-        'Spring', 'Summer', 'Moonlight', 'Winding', 'Windy', 'Whispering',
-        'Falling', 'Roaring', 'Hummingbird', 'Mockingbird', 'Bluebird',
-        'Robin', 'Babbling', 'Cedar', 'Pine', 'Ash', 'Maple', 'Oak', 'Birch',
-        'Cherry', 'Blossom', 'Rosewood', 'Apple', 'Peach', 'Blackberry',
-        'Strawberry', 'Starlight', 'Wilderness', 'Dappled', 'Beaver', 'Acorn',
-        'Pecan', 'Pheasant', 'Owl'
-    ])
-    text_2 = choice([
-        'Way', 'Lane', 'Boulevard', 'Street', 'Drive', 'Circle', 'Avenue',
-        'Trail',
-    ])
-    return '%d %s %s' % (number, text_1, text_2)
-
-
-city = partial(choice, ['Boston', 'Cambridge', ])
-country = partial(choice, ['USA', 'UK'])
-
-people = [
-    Person(
-        name='%s %s' % (any_name(), family_name()),
-        age=age(),
-        favorite_color=favorite_color(),
-        address=Address(
-            street=street(),
-            city=city(),
-            country=country(),
-        ),
-    )
-    for i in range(100)
-]
-
-
 # Application entry point.
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+
     # Create the GUI (this does NOT start the GUI event loop).
     gui = GUI()
 
     # Create and open the main window.
-    window = MainWindow(data=people, row_info=row_info)
+    window = MainWindow(row_info=row_info)
     window.open()
 
     # Start the GUI event loop!
     gui.start_event_loop()
+    logger.info("Shutting down")
