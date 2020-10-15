9aa64ba394b3d234f96641172b4c2d105424fac0
Corran Webster <cwebster@enthought.com>
2020-10-15 15:08:30 +0100

    Add some extra imports, change import locations.

--- pyface/data_view/api.py
+++ pyface/data_view/api.py
@@ -19,7 +19,7 @@ from pyface.data_view.data_formats import (  # noqa: 401
     csv_column_format, csv_format, csv_row_format, from_csv, from_csv_column,
     from_csv_row, from_json, from_npy, html_format, npy_format,
     standard_text_format, to_csv, to_csv_column, to_csv_row, to_json, to_npy,
-    table_format, text_column_format, text_format, text_row_format
+    table_format, text_column_format, text_row_format
 )
 from pyface.data_view.data_view_errors import (  # noqa: 401
     DataViewError, DataViewGetError, DataViewSetError
@@ -29,7 +29,9 @@ from pyface.data_view.data_wrapper import DataWrapper  # noqa: 401
 from pyface.data_view.i_data_view_widget import (  # noqa: 401
     IDataViewWidget, MDataViewWidget
 )
-from pyface.data_view.i_data_wrapper import IDataWrapper, MDataWrapper  # noqa: 401
+from pyface.data_view.i_data_wrapper import (  # noqa: 401
+    DataFormat, IDataWrapper, MDataWrapper, text_format
+)
 from pyface.data_view.index_manager import (  # noqa: 401
     AbstractIndexManager, IntIndexManager, TupleIndexManager
 )
