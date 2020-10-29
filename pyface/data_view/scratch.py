131a826f186d8e9f2bbb44cb5dd60959b27d5fac
Kit Yan Choi <kchoi@enthought.com>
2020-10-29 17:31:05 +0000

    Get the checkstate working

--- pyface/data_view/scratch.py
+++ pyface/data_view/scratch.py
@@ -1,11 +1,15 @@
+import enum
+import logging
 
-from traits.api import HasStrictTraits
+from traits.api import Bool, List, Instance, HasStrictTraits, Dict, Callable
 
 from pyface.api import ApplicationWindow, GUI
 
+
+from pyface.data_view.api import DataViewSetError, DataViewGetError
+from pyface.data_view.abstract_value_type import CheckState
 from pyface.data_view.data_models.data_accessors import AttributeDataAccessor
 from pyface.data_view.data_models.row_table_data_model import RowTableDataModel
-
 from pyface.data_view.value_types.api import TextValue   # placeholder for now.
 
 # Use Qt implementations for proof-of-concept purposes.
@@ -14,6 +18,8 @@ from pyface.qt.QtCore import Qt
 from pyface.ui.qt4.data_view.data_view_item_model import DataViewItemModel
 from pyface.ui.qt4.data_view.data_view_widget import DataViewWidget
 
+
+logger = logging.getLogger(__name__)
 
 
 class DataItem:
@@ -41,16 +47,47 @@ class ItemDelegate(HasStrictTraits):
     In Qt world, this maps to QAbstractItemDelegate, plus the different
     handling to do with different role flags in the view.
     """
-    pass
+
+    # Callable(model, row, column) -> boolean
+    is_delegate_for = Callable()
+
+    # Callable(any) -> str
+    to_text = Callable(default_value=str, allow_none=True)
+
+    # Callable(any) -> CheckState
+    to_check_state = Callable(default_value=None, allow_none=True)
+
+    # Callable(CheckState) -> any
+    from_check_state = Callable(default_value=None, allow_none=True)
 
 
 class NewDataViewItemModel(DataViewItemModel):
-    """ Override functionality of DataViewItemModel."""
+    """ Override functionality of Qt DataViewItemModel."""
+
+    set_check_state_map = {
+        Qt.Checked: CheckState.CHECKED,
+        Qt.Unchecked: CheckState.UNCHECKED,
+    }
+    get_check_state_map = {
+        CheckState.CHECKED: Qt.Checked,
+        CheckState.UNCHECKED: Qt.Unchecked,
+    }
+
+    def __init__(
+        self, model, selection_type, exporters, parent=None,
+        delegates=None,
+    ):
+        """ Reimplemented to allow a list of delegates to be used by the
+        model.
+        """
+        super().__init__(model, selection_type, exporters, parent)
+        # The delegates include logic previously supported by ValueType.
+        self.delegates = [] if delegates is None else delegates
 
     def flags(self, index):
+        """ Reimplemented flags to use delegates instead of ValueType."""
         row = self._to_row_index(index)
         column = self._to_column_index(index)
-        value_type = self.model.get_value_type(row, column)
         if row == () and column == ():
             return Qt.ItemIsEnabled
 
@@ -58,38 +95,88 @@ class NewDataViewItemModel(DataViewItemModel):
         if is_qt5 and not self.model.can_have_children(row):
             flags |= Qt.ItemNeverHasChildren
 
+        delegate = self._get_delegate(row, column)
+        if delegate.to_check_state is not None:
+            flags |= Qt.ItemIsEditable
+            flags |= Qt.ItemIsUserCheckable
+        return flags
+
+    def data(self, index, role=Qt.DisplayRole):
+        """ Reimplemented data to use delegates instead of ValueType."""
+        row = self._to_row_index(index)
+        column = self._to_column_index(index)
+        value = self.model.get_value(row, column)
+
+        delegate = self._get_delegate(row, column)
+
+        if role == Qt.DisplayRole and delegate.to_text is not None:
+            return delegate.to_text(value)
+
+        if role == Qt.CheckStateRole and delegate.to_check_state is not None:
+            return self.get_check_state_map[delegate.to_check_state(value)]
+        return None
+
+    def setData(self, index, value, role=Qt.EditRole):
+        """ Reimplemented setData to use delegates instead of ValueType."""
+        row = self._to_row_index(index)
+        column = self._to_column_index(index)
+
+        delegate = self._get_delegate(row, column)
+
+        if role == Qt.CheckStateRole and delegate.from_check_state is not None:
+            value = self.set_check_state_map[value]
+            return self._set_value_with_exception(
+                row=row, column=column,
+                value=delegate.from_check_state(value)
+            )
+
+        return False
+
+    # Private methods
+
+    def _get_delegate(self, row, column):
+        """ Find the delegate for the given row and column index."""
+        for delegate in self.delegates:
+            if delegate.is_delegate_for(self.model, row, column):
+                return delegate
+        return ItemDelegate()
+
+    def _set_value_with_exception(self, row, column, value):
+        """ Set model value with exception handling.
+
+        Returns
+        -------
+        is_successful : bool
+            If setting the value was successful.
+        """
         try:
-            if value_type:
-                if value_type.has_editor_value(self.model, row, column):
-                    flags |= Qt.ItemIsEditable
-                if (
-                    value_type.has_check_state(self.model, row, column)
-                    and self.model.can_set_value(row, column)
-                ):
-                    flags |= Qt.ItemIsUserCheckable
-        except DataViewGetError:
-            # expected error, ignore
-            pass
+            self.model.set_value(row, column, value)
+        except DataViewSetError:
+            return False
         except Exception:
-            # unexpected error, log and raise
+            # unexpected error, log and persevere
             logger.exception(
-                "get flags failed: row %r, column %r",
+                "setData failed: row %r, column %r, value %r",
                 row,
                 column,
+                value,
             )
-            raise
-
-        return flags
+            return False
+        else:
+            return True
 
 
 class NewDataViewWidget(DataViewWidget):
     """ Override to provide a different DataViewItemModel."""
+
+    delegates = List(Instance(ItemDelegate))
 
     def _create_item_model(self):
         self._item_model = NewDataViewItemModel(
             self.data_model,
             self.selection_type,
             self.exporters,
+            delegates=self.delegates,
         )
 
 
@@ -98,12 +185,31 @@ class NewDataViewWidget(DataViewWidget):
 # --------------------------------------------------------------------
 
 
+def basic_to_check_state(value):
+    if bool(value):
+        return CheckState.CHECKED
+    return CheckState.UNCHECKED
+
+
+def basic_from_check_state(value):
+    return value == CheckState.CHECKED
+
+
 class MainWindow(ApplicationWindow):
 
     def _create_contents(self, parent):
         widget = NewDataViewWidget(
             parent=parent,
             data_model=create_model(),
+            delegates=[
+                ItemDelegate(
+                    is_delegate_for=(
+                        lambda model, row, column: column == (1, )
+                    ),
+                    to_check_state=basic_to_check_state,
+                    from_check_state=basic_from_check_state,
+                )
+            ]
         )
         widget._create()
         return widget.control
@@ -112,13 +218,13 @@ class MainWindow(ApplicationWindow):
 def create_model():
     objects = [
         DataItem(
-            a="Hello", b="World", c=3,
+            a="Hello", b="World", c=True,
         ),
         DataItem(
-            a=2, b=3, c=4,
+            a=2, b=3, c=True,
         ),
         DataItem(
-            a=2, b=3, c=4,
+            a=2, b=3, c=True,
         ),
     ]
     column_data = [
