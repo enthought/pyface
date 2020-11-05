c7cca021690839ed8e00dfbebe25a49180f06823
Sai Rahul Poruri <rporuri@enthought.com>
2020-11-05 09:14:44 +0000

    DEV : Add importlib-metadata to dependencies
    	modified:   pyface/__init__.py

--- pyface/__init__.py
+++ pyface/__init__.py
@@ -18,7 +18,7 @@ except ImportError:
     __version__ = "not-built"
 
 
-__requires__ = ["traits>=6"]
+__requires__ = ["importlib-metadata", "traits>=6"]
 __extras_require__ = {
     "wx": ["wxpython>=4", "numpy"],
     "pyqt": ["pyqt>=4.10", "pygments"],
