d3f9133bb4071c8dbd875f5c62c928643843e5df
Aaron Ayres <aayres@enthought.com>
2020-11-25 14:59:28 -0700

    copy Undo over from apptools

---
+++ pyface/undo/__init__.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2005-2011, Enthought, Inc.
+# All rights reserved.
+#
+# This software is provided without warranty under the terms of the BSD
+# license included in LICENSE.txt and may be redistributed only
+# under the conditions described in the aforementioned license.  The license
+# is also available online at http://www.enthought.com/licenses/BSD.txt
+# Thanks for using Enthought open source!
+#
+# Author: Enthought, Inc.
+""" Supports undoing and scripting application commands.
+    Part of the AppTools project of the Enthought Tool Suite.
+"""
---
+++ pyface/undo/action/api.py
@@ -0,0 +1,17 @@
+# ------------------------------------------------------------------------------
+# Copyright (c) 2007, Riverbank Computing Limited
+# All rights reserved.
+#
+# This software is provided without warranty under the terms of the BSD
+# license included in enthought/LICENSE.txt and may be redistributed only
+# under the conditions described in the aforementioned license.  The license
+# is also available online at http://www.enthought.com/licenses/BSD.txt
+# Thanks for using Enthought open source!
+#
+# Author: Riverbank Computing Limited
+# Description: <Enthought undo package component>
+# ------------------------------------------------------------------------------
+
+from .command_action import CommandAction
+from .redo_action import RedoAction
+from .undo_action import UndoAction
