4a991afb5eb243af5d303b7281b906f6bf24e109
Poruri Sai Rahul <rporuri@enthought.com>
2020-12-16 09:57:35 +0000

    Address a few sphinx warnings (#846)
    * CLN : Address a few sphinx warnings
    
    Not sure if this is the right fix but this is a fix
    
    	modified:   pyface/tasks/tasks_application.py
    	modified:   pyface/timer/do_later.py
    
    * Update pyface/timer/do_later.py
    
    Co-authored-by: Kit Choi <kitchoi@users.noreply.github.com>
    
    Co-authored-by: Kit Choi <kitchoi@users.noreply.github.com>

--- pyface/timer/do_later.py
+++ pyface/timer/do_later.py
@@ -38,7 +38,7 @@ def do_later(callable, *args, **kwargs):
     ----------
     callable : callable
         The callable to call in 50ms time.
-    *args, **kwargs : tuple, dict
+    *args, \**kwargs : tuple, dict
         Arguments to be passed through to the callable.
     """
     return CallbackTimer.single_shot(
@@ -55,7 +55,10 @@ def do_after(interval, callable, *args, **kwargs):
         The time interval in milliseconds to wait before calling.
     callable : callable
         The callable to call.
-    *args, **kwargs : tuple, dict
+    \*args
+        Positional arguments to be passed through to the callable.
+    \*\*kwargs
+        Keyword arguments to be passed through to the callable.
         Arguments to be passed through to the callable.
     """
     return CallbackTimer.single_shot(
