9552c9b502e610d64d727c7330616dade54ee943
Aaron Ayres <aayres@enthought.com>
2021-02-01 11:03:57 -0600

    keep it simple

--- pyface/timer/tests/test_timer.py
+++ pyface/timer/tests/test_timer.py
@@ -25,7 +25,7 @@ class ConditionHandler(object):
         self.times = []
         self.called = False
 
-    def callback(self, event):
+    def callback(self, event=None):
         self.times.append(perf_counter())
         self.count += 1
         self.called = True
