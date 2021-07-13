131a3ce404ce59d93092196c3e8d816b8c8613c2
Corran Webster <cwebster@enthought.com>
2021-07-13 13:27:26 +0100

    SplitWidget fixes (#977)
    * Improvements to SplitWidget/SplitPanel:
    
    - clean up of traits (including adding an "Orientation" trait)
    - deprecate automatic creation of widgets in __init__
    - general clean-up
    
    * Drive-by conversion to new Orientation trait.
    
    * Raise if creation methods not implemented.

--- pyface/ui/qt4/split_widget.py
+++ pyface/ui/qt4/split_widget.py
@@ -11,34 +11,20 @@
 # This software is provided without warranty under the terms of the BSD license.
 # However, when used with the GPL version of PyQt the additional terms described in the PyQt GPL exception also apply
 
-
 """ Mix-in class for split widgets. """
-
 
 from pyface.qt import QtCore, QtGui
 
-
-from traits.api import Callable, Enum, Float, HasTraits, provides
-
+from traits.api import provides
 
 from pyface.i_split_widget import ISplitWidget, MSplitWidget
 
 
 @provides(ISplitWidget)
-class SplitWidget(MSplitWidget, HasTraits):
+class SplitWidget(MSplitWidget):
     """ The toolkit specific implementation of a SplitWidget.  See the
     ISPlitWidget interface for the API documentation.
     """
-
-    # 'ISplitWidget' interface ---------------------------------------------
-
-    direction = Enum("vertical", "vertical", "horizontal")
-
-    ratio = Float(0.5)
-
-    lhs = Callable
-
-    rhs = Callable
 
     # ------------------------------------------------------------------------
     # Protected 'ISplitWidget' interface.
