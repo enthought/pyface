2ab1d35bafad6f440590dcafdf2e954a272401fc
Corran Webster <cwebster@enthought.com>
2021-07-13 13:23:30 +0100

    HeadingText fixes (#976)
    * Improvements and fixes for HeadingText.
    
    - aligns Qt _create_control with standard API
    - route creation throught create() call
    - make widget creation in init() optional
    - deprecate automatic widget creation
    - various other small fixes
    
    * Refactor HeadingText so that:
    
    - appearance of Qt and Wx are similar
    - changes to text are dispatched on the ui thread
    - general clean-up and standardization
    
    * Remove debugging print statement.

--- pyface/ui/qt4/heading_text.py
+++ pyface/ui/qt4/heading_text.py
@@ -11,12 +11,9 @@
 # This software is provided without warranty under the terms of the BSD license.
 # However, when used with the GPL version of PyQt the additional terms described in the PyQt GPL exception also apply
 
-
 from pyface.qt import QtGui
 
-
-from traits.api import Int, observe, provides, Str
-
+from traits.api import provides
 
 from pyface.i_heading_text import IHeadingText, MHeadingText
 from .widget import Widget
@@ -24,56 +21,34 @@ from .widget import Widget
 
 @provides(IHeadingText)
 class HeadingText(MHeadingText, Widget):
-    """ The toolkit specific implementation of a HeadingText.  See the
-    IHeadingText interface for the API documentation.
+    """ The Qt-specific implementation of a HeadingText.
     """
 
-    # 'IHeadingText' interface ---------------------------------------------
-
-    level = Int(1)
-
-    text = Str("Default")
-
     # ------------------------------------------------------------------------
-    # 'object' interface.
+    # 'IWidget' interface.
     # ------------------------------------------------------------------------
 
-    def __init__(self, parent, **traits):
-        """ Creates the panel. """
-
-        # Base class constructor.
-        super().__init__(**traits)
-
-        # Create the toolkit-specific control that represents the widget.
-        self._create_control(parent)
+    def _create_control(self, parent):
+        """ Create the toolkit-specific control that represents the widget. """
+        control = QtGui.QLabel(parent)
+        control.setSizePolicy(
+            QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Fixed
+        )
+        return control
 
     # ------------------------------------------------------------------------
     # Private interface.
     # ------------------------------------------------------------------------
 
-    def _create_control(self, parent):
-        """ Create the toolkit-specific control that represents the widget. """
-
-        self.control = QtGui.QLabel(parent)
-        self._set_text(self.text)
-
-        self.control.setSizePolicy(
-            QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Fixed
-        )
-
-    def _set_text(self, text):
+    def _set_control_text(self, text):
         """ Set the text on the toolkit specific widget. """
-
         # Bold the text. Qt supports a limited subset of HTML for rich text.
-        text = "<b>" + text + "</b>"
-
+        text = f"<b>{text}</b>"
         self.control.setText(text)
 
-    # Trait event handlers -------------------------------------------------
-
-    @observe("text")
-    def _update_text(self, event):
-        """ Called when the text is changed. """
-        new = event.new
-        if self.control is not None:
-            self._set_text(new)
+    def _get_control_text(self):
+        """ Get the text on the toolkit specific widget. """
+        text = self.control.text()
+        # remove the bolding from the text
+        text = text[3:-4]
+        return text
