2ad085fd6c97d24428a4029b82509c264976e660
Corran Webster <cwebster@enthought.com>
2022-07-08 15:34:09 +0100

    Add a regression test for #1147.

--- pyface/ui/qt4/console/tests/test_console_widget.py
+++ pyface/ui/qt4/console/tests/test_console_widget.py
@@ -10,7 +10,8 @@
 
 import unittest
 
-from pyface.qt.QtGui import QApplication
+from pyface.qt import QtCore
+from pyface.qt.QtGui import QApplication, QKeyEvent
 
 from ..console_widget import ConsoleWidget
 
@@ -33,3 +34,16 @@ class TestConsoleWidget(unittest.TestCase):
             text,
             "one  two  three\n"
         )
+
+    def test_right_key_press(self):
+        # regression test for Pyface #1147
+        widget = ConsoleWidget()
+        event = QKeyEvent(
+            QtCore.QEvent.Type.KeyPress,
+            QtCore.Qt.Key.Key_Right,
+            QtCore.Qt.KeyboardModifier.NoModifier,
+        )
+
+        intercepted = widget._event_filter_console_keypress(event)
+
+        self.assertTrue(intercepted)
