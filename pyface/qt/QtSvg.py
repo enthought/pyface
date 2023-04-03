3e9c43b216d09d072ba723094074883f0cdd1605
Corran Webster <cwebster@enthought.com>
2023-04-03 16:06:05 +0100

    Update Qt APIs to be more Qt5/Qt6-ish
    In particular this includes QtSvgWidgets, and backwards
    compatibility imports in QtSvg on Qt6 platforms.

--- pyface/qt/QtSvg.py
+++ pyface/qt/QtSvg.py
@@ -9,17 +9,18 @@
 # Thanks for using Enthought open source!
 from . import qt_api
 
-if qt_api == "pyqt":
-    from PyQt4.QtSvg import *
-
-elif qt_api == "pyqt5":
+if qt_api == "pyqt5":
     from PyQt5.QtSvg import *
 
 elif qt_api == "pyqt6":
     from PyQt6.QtSvg import *
+    # Backwards compatibility imports
+    from PyQt6.QtSvgWidgets import *
 
 elif qt_api == "pyside6":
     from PySide6.QtSvg import *
+    # Backwards compatibility imports
+    from PySide6.QtSvgWidgets import *
 
 else:
     from PySide2.QtSvg import *
